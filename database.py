3a46c8579e68453f0bcc530185f744c6d8ca5d52
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-06-02 11:39:33 +0300

    -setting up database.py for the db connections and setting up alembic editing the alembic.ini and envy file

---
+++ database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker,declarative_base
+
+
+DATABASE_URL= "sqlite:///permit_tracker.db"
+
+engine=create_engine(DATABASE_URL,)
+Session= sessionmaker(bind=engine)
+session= Session()
+
+Base= declarative_base()
